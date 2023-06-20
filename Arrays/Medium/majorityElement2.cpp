7dc49258e2385c8674d5a261095807a585aeb89d
Vishal Chauhan <vsc258srt125vv@gmail.com>
2023-06-20 20:22:55 +0530

    Majority element n/3 added

---
+++ Arrays/Medium/majorityElement2.cpp
@@ -0,0 +1,150 @@
+// Majority Element (n/3) times
+// Brute Force
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> majorityElement(vector<int> v) {
+    int n = v.size(); //size of the array
+    vector<int> ls; // list of answers
+
+    for (int i = 0; i < n; i++) {
+        //selected element is v[i]:
+        // Checking if v[i] is not already
+        // a part of the answer:
+        if (ls.size() == 0 || ls[0] != v[i]) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                // counting the frequency of v[i]
+                if (v[j] == v[i]) {
+                    cnt++;
+                }
+            }
+
+            // check if frequency is greater than n/3:
+            if (cnt > (n / 3))
+                ls.push_back(v[i]);
+        }
+
+        if (ls.size() == 2) break;
+    }
+
+    return ls;
+}
+
+int main()
+{
+    vector<int> arr = {1,1,1,3,3,2,2,2};
+    vector<int> ans = majorityElement(arr);
+    cout << "The majority elements are: ";
+    for (auto it : ans)
+        cout << it << " ";
+    cout << "\n";
+    return 0;
+}
+
+
+// Optimal Solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> majorityElement(vector<int> v) {
+    int n = v.size(); //size of the array
+    vector<int> ls; // list of answers
+
+    //declaring a map:
+    map<int, int> mpp;
+
+    // least occurrence of the majority element:
+    int mini = int(n / 3) + 1;
+
+    //storing the elements with its occurnce:
+    for (int i = 0; i < n; i++) {
+        mpp[v[i]]++;
+
+        //checking if v[i] is
+        // the majority element:
+        if (mpp[v[i]] == mini) {
+            ls.push_back(v[i]);
+        }
+        if (ls.size() == 2) break;
+    }
+
+    return ls;
+}
+
+// T.C => O(n)
+// S.C => O(1)
+
+int main()
+{
+    vector<int> arr = {11, 33, 33, 11, 33, 11};
+    vector<int> ans = majorityElement(arr);
+    cout << "The majority elements are: ";
+    for (auto it : ans)
+        cout << it << " ";
+    cout << "\n";
+    return 0;
+}
+
+
+// Most Optimized Solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> majorityElement(vector<int> v) {
+    int n = v.size(); //size of the array
+
+    int cnt1 = 0, cnt2 = 0; // counts
+    int el1 = INT_MIN; // element 1
+    int el2 = INT_MIN; // element 2
+
+    // applying the Extended Boyer Moore's Voting Algorithm:
+    for (int i = 0; i < n; i++) {
+        if (cnt1 == 0 && el2 != v[i]) {
+            cnt1 = 1;
+            el1 = v[i];
+        }
+        else if (cnt2 == 0 && el1 != v[i]) {
+            cnt2 = 1;
+            el2 = v[i];
+        }
+        else if (v[i] == el1) cnt1++;
+        else if (v[i] == el2) cnt2++;
+        else {
+            cnt1--, cnt2--;
+        }
+    }
+
+    vector<int> ls; // list of answers
+
+    // Manually check if the stored elements in
+    // el1 and el2 are the majority elements:
+    cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == el1) cnt1++;
+        if (v[i] == el2) cnt2++;
+    }
+
+    int mini = int(n / 3) + 1;
+    if (cnt1 >= mini) ls.push_back(el1);
+    if (cnt2 >= mini) ls.push_back(el2);
+
+    // T.C => O(2N)
+    // S.C => O(1)
+
+    return ls;
+}
+
+int main()
+{
+    vector<int> arr = {11, 33, 33, 11, 33, 11};
+    vector<int> ans = majorityElement(arr);
+    cout << "The majority elements are: ";
+    for (auto it : ans)
+        cout << it << " ";
+    cout << "\n";
+    return 0;
+}
