c9f4fd411cf001b5680a870c8bf2d580c134d046
Vishal Chauhan <vsc258srt125vv@gmail.com>
2023-06-23 21:57:10 +0530

    update reversePair.cpp

--- Arrays/Hard/reversePairs.cpp
+++ Arrays/Hard/reversePairs.cpp
@@ -4,30 +4,30 @@
 
 // ans = 15
 
-// Brute Force
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main()
-// {
-//     int arr[] = {40,25,19,12,9,6,2};
-//     int n = sizeof(arr)/sizeof(arr[0]);
+Brute Force
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[] = {40,25,19,12,9,6,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-//     int cnt = 0;
-//     for(int i = 0; i < n; ++i)
-//     {
-//         for(int j = i + 1; j < n; ++j)
-//         {
-//             if(arr[i] > 2 * arr[j])
-//             {
-//                 cnt++;
-//             }
-//         }
-//     }
+    int cnt = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = i + 1; j < n; ++j)
+        {
+            if(arr[i] > 2 * arr[j])
+            {
+                cnt++;
+            }
+        }
+    }
 
-//     cout << cnt << endl;
+    cout << cnt << endl;
 
-//     return 0;
-// }
+    return 0;
+}
 
 // Optimal Approach
 
