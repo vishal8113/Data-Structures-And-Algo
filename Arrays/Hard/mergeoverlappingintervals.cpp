698c38af5d58308d7902f1ba338b5567605b8620
Vishal Chauhan <vsc258srt125vv@gmail.com>
2023-06-23 20:24:16 +0530

    merge overlapping subIntervals code added

---
+++ Arrays/Hard/mergeoverlappingintervals.cpp
@@ -0,0 +1,96 @@
+// ex = {(1,3),(2,6),(8,9),(9,11),(8,10),(2,4),(15,18),(16,17)}
+
+// ans = {1,6} {8,11} {15,18}
+
+// Brute Force
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr) {
+    int n = arr.size(); // size of the array
+
+    //sort the given intervals:
+    sort(arr.begin(), arr.end());
+
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++) { // select an interval:
+        int start = arr[i][0];
+        int end = arr[i][1];
+
+        //Skip all the merged intervals:
+        if (!ans.empty() && end <= ans.back()[1]) {
+            continue;
+        }
+
+        //check the rest of the intervals:
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j][0] <= end) {
+                end = max(end, arr[j][1]);
+            }
+            else {
+                break;
+            }
+        }
+        ans.push_back({start, end});
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<vector<int>> arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+    vector<vector<int>> ans = mergeOverlappingIntervals(arr);
+    for (auto it : ans) {
+        cout << "[" << it[0] << ", " << it[1] << "] ";
+    }
+    cout << endl;
+
+    // T.C => O(N*logN) + O(2*N)
+    // S.C => O(N)
+    return 0;
+}
+
+
+// Optimal Approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr) {
+    int n = arr.size(); // size of the array
+
+    //sort the given intervals:
+    sort(arr.begin(), arr.end());
+
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++) {
+        // if the current interval does not
+        // lie in the last interval:
+        if (ans.empty() || arr[i][0] > ans.back()[1]) {
+            ans.push_back(arr[i]);
+        }
+        // if the current interval
+        // lies in the last interval:
+        else {
+            ans.back()[1] = max(ans.back()[1], arr[i][1]);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<vector<int>> arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+    vector<vector<int>> ans = mergeOverlappingIntervals(arr);
+    for (auto it : ans) {
+        cout << "[" << it[0] << ", " << it[1] << "] ";
+    }
+    cout << endl;
+
+    // T.C => O(N*logN) + O(N)
+    // S.C => O(N)
+    return 0;
+}
