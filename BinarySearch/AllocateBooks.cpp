804b86f9327c985760a5f0aa26262934da88b5e1
Vishal Chauhan <vsc258srt125vv@gmail.com>
2023-07-01 20:19:57 +0530

    update

--- BinarySearch/AllocateBooks.cpp
+++ BinarySearch/AllocateBooks.cpp
@@ -2,76 +2,76 @@
 
 // Brute Force
 
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-// int maxi(int* arr,int n)
-// {
-//     int k = arr[0];
-//     for(int i = 1;i < n; ++i)
-//     {
-//         if(arr[i] > k)
-//         {
-//             k = arr[i];
-//         }
-//     }
+int maxi(int* arr,int n)
+{
+    int k = arr[0];
+    for(int i = 1;i < n; ++i)
+    {
+        if(arr[i] > k)
+        {
+            k = arr[i];
+        }
+    }
 
-//     return k;
-// }
+    return k;
+}
 
-// int fn(int* arr,int n,int pages)
-// {
-//     int student = 1;
-//     int pageHold = 0;
+int fn(int* arr,int n,int pages)
+{
+    int student = 1;
+    int pageHold = 0;
 
-//     for(int i = 0; i < n; ++i)
-//     {
-//         if(pageHold + arr[i] <= pages)
-//         {
-//             pageHold+=arr[i];
-//         }else{
-//             student++;
-//             pageHold = arr[i];
-//         }
-//     }
+    for(int i = 0; i < n; ++i)
+    {
+        if(pageHold + arr[i] <= pages)
+        {
+            pageHold+=arr[i];
+        }else{
+            student++;
+            pageHold = arr[i];
+        }
+    }
 
-//     return student;
-// }
+    return student;
+}
 
-// int main()
-// {
-//     int arr[] = {25,46,28,49,24};
-//     int n = sizeof(arr)/sizeof(arr[0]);
+int main()
+{
+    int arr[] = {25,46,28,49,24};
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-//     int students = 4;
-//     int ans = -1;
+    int students = 4;
+    int ans = -1;
 
-//     if(students > n)
-//     {
-//         ans = -1;
-//     }
+    if(students > n)
+    {
+        ans = -1;
+    }
 
-//     int maxEle = maxi(arr,n);
+    int maxEle = maxi(arr,n);
 
-//     int sum = accumulate(arr,arr+n,0);
+    int sum = accumulate(arr,arr+n,0);
 
-//     for(int pages = maxEle; pages <= sum; ++pages)
-//     {
-//         int cntStudent = fn(arr,n,pages);
+    for(int pages = maxEle; pages <= sum; ++pages)
+    {
+        int cntStudent = fn(arr,n,pages);
 
-//         if(cntStudent == students)
-//         {
-//             ans = pages;
-//             break;
-//         }
-//     }
+        if(cntStudent == students)
+        {
+            ans = pages;
+            break;
+        }
+    }
 
-//     // T.C => O(sum-maxi+1) * O(n)
-//     // S.C => O(1)
+    // T.C => O(sum-maxi+1) * O(n)
+    // S.C => O(1)
 
-//     cout << ans << endl;
-//     return 0;
-// }
+    cout << ans << endl;
+    return 0;
+}
 
 // Optimal (Binary search)
 
@@ -144,6 +144,9 @@ int main()
     }
 
     cout << start << endl;
+
+    // T.C => O(log2(sum - maxi + 1) * n)
+    // S.C => O(1)
 
     return 0;
 }
