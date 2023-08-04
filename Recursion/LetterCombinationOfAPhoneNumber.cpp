00b77d29d6981ed071bb3d06d6de056c4dac8d73
Vishal Chauhan <vsc258srt125vv@gmail.com>
2023-08-04 19:48:00 +0530

    Letter combinations of a phone number code added

---
+++ Recursion/LetterCombinationOfAPhoneNumber.cpp
@@ -0,0 +1,34 @@
+// Letter Combination of a phone number
+
+class Solution {
+public:
+void findCombination(string digits,string output,int index,vector<string> &ans,string mapping[]){
+        if(index == digits.length()){
+        ans.push_back(output);
+        return;
+    }
+
+    int number = digits[index] -'0';
+    string value = mapping[number];
+
+    for(int i = 0; i < value.length(); ++i)
+    {
+        output.push_back(value[i]);
+        findCombination(digits,output,index+1,ans,mapping);
+        output.pop_back();
+    }
+}    
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.length() == 0){
+            return ans;
+        }
+        string output;
+        int index = 0;
+        string mapping[10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+        findCombination(digits,output,index,ans,mapping);
+        return ans;
+    }
+};
